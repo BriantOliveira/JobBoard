723a9e1db6f61fc1de12d9a6afaa3b4ba51fb409
Briant Oliveira <briantmoliveira@gmail.com>
2018-11-26 10:09:35 -0800

    Deleting active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120012115) do
+ActiveRecord::Schema.define(version: 20181120012914) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20181120012115) do
     t.string "card_exp_month"
     t.string "card_exp_year"
     t.datetime "expires_at"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
