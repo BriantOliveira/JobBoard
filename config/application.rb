2225060ba70f6ba35585de8e671c525bc6d4c3e9
Briant Oliveira <briantmoliveira@gmail.com>
2018-11-26 18:10:41 -0800

    moved config

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module JobBoard
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
@@ -13,6 +13,7 @@ module JobBoard
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
